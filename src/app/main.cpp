97107fa1eb8b7895f618530ce280d7001f29ac40
man <tmandalo@mans-MacBook-Pro.local>
2022-07-31 08:37:57 +0300

    commit for commmit

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 
-namespace std;
+using namespace std;
 
-int main()
-{
+int input(int x, int y){
+    cout << "X: ";
+    cin >> x;
+    cout << "\nY: ";
+    cin >> y;
+    return x, y;
+}
 
+void print_matrix(){
+    cout << "|1, 1|1, 2|1, 3|"<< endl;
+    cout << "|2, 1|2, 2|2, 3|"<< endl;
+    cout << "|3, 1|3, 2|3, 3|"<< endl;
+}
+
+int main(){
+    input(x, y);
+    print_matrix();
 }
